5d385e29c8a92f27fb6aff72a023ddcc4cdd94c5
TheDarkLord100 <miracleyt404@gmail.com>
2022-12-03 10:24:08 +0530

    Added programs part

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -37,8 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       body: ListView(
-        padding: const EdgeInsets.only(left: 18, right: 18,
-        top: 20),
+        padding: const EdgeInsets.only(left: 18, right: 18, top: 20),
         children: [
           Text(
             'Hello, Priya!',
@@ -49,8 +48,7 @@ class _HomeScreenState extends State<HomeScreen> {
             style: GoogleFonts.inter(
                 fontWeight: FontWeight.bold,
                 fontSize: 12,
-              color: const Color(0xFF6D747A)
-            ),
+                color: const Color(0xFF6D747A)),
           ),
           const SizedBox(
             height: 30,
@@ -71,98 +69,253 @@ class _HomeScreenState extends State<HomeScreen> {
                     padding: const EdgeInsets.only(left: 20),
                     margin: const EdgeInsets.all(5),
                     decoration: BoxDecoration(
-                      border: Border.all(
-                        color: const Color(0xFF598BED),
+                        border: Border.all(
+                          color: const Color(0xFF598BED),
+                        ),
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const <Widget>[
+                        Icon(
+                          Icons.book_outlined,
+                          color: Color(0xFF598BED),
+                        ), // ic
+                        SizedBox(
+                          width: 10,
+                        ), // on
+                        Text(
+                          "Programs",
+                          style: TextStyle(color: Color(0xFF598BED)),
+                        ), // text
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 20),
+                    margin: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color(0xFF598BED),
+                        ),
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const <Widget>[
+                        Icon(
+                          Icons.help,
+                          color: Color(0xFF598BED),
+                        ), // ic
+                        SizedBox(
+                          width: 10,
+                        ), // on
+                        Text(
+                          "Get Help",
+                          style: TextStyle(color: Color(0xFF598BED)),
+                        ), // text
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 20),
+                    margin: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color(0xFF598BED),
+                        ),
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const <Widget>[
+                        Icon(
+                          Icons.import_contacts,
+                          color: Color(0xFF598BED),
+                        ), // ic
+                        SizedBox(
+                          width: 10,
+                        ), // on
+                        Text(
+                          "Learn",
+                          style: TextStyle(color: Color(0xFF598BED)),
+                        ), // text
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 20),
+                    margin: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color(0xFF598BED),
+                        ),
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const <Widget>[
+                        Icon(
+                          Icons.insert_chart,
+                          color: Color(0xFF598BED),
+                        ), // ic
+                        SizedBox(
+                          width: 10,
+                        ), // on
+                        Text(
+                          "DD Tracker",
+                          style: TextStyle(color: Color(0xFF598BED)),
+                        ), // text
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Programs for you',
+                style:
+                    GoogleFonts.lora(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Text(
+                      'View all  ',
+                      style: GoogleFonts.inter(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: const Color(0xFF6D747A)),
+                    ),
+                    const Icon(
+                      Icons.arrow_right_alt_outlined,
+                      color: Color(0xFF6D747A),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 300,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  width: 240,
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: const Color(0xFFEBEDF0)),
+                      color: const Color(0xFFFFFFFF),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0xFF0E443E),
+                          blurRadius: 4.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Image(
+                        image: AssetImage('assets/card 1.png'),
+                        fit: BoxFit.fill,
+                        height: 140,
                       ),
-                      borderRadius: BorderRadius.circular(8)
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: const <Widget>[
-                        Icon(Icons.book_outlined,
-                        color: Color(0xFF598BED),), // ic
-                        SizedBox(width: 10,),// on
-                        Text("Programs",
-                        style: TextStyle(
-                          color: Color(0xFF598BED)
-                        ),), // text
-                      ],
-                    ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10.0),
+                        child: Text(
+                          'LIFESTYLE',
+                          style:
+                              TextStyle(fontSize: 12, color: Color(0xFF5988ED)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Flexible(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 10.0, right: 10),
+                          child: Text(
+                          'A complete guide for your new born baby',
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+                      Padding(padding: EdgeInsets.only(left: 10),
+                        child: Text('16 lessons', style: TextStyle(fontSize: 12, color: Color(0xFF6D747A)),),
+                      )
+                    ],
                   ),
                 ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    padding: const EdgeInsets.only(left: 20),
-                    margin: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          color: const Color(0xFF598BED),
+                Container(
+                  width: 240,
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: const Color(0xFFEBEDF0)),
+                      color: const Color(0xFFFFFFFF),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0xFF0E443E),
+                          blurRadius: 4.0,
                         ),
-                        borderRadius: BorderRadius.circular(8)
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: const <Widget>[
-                        Icon(Icons.help,
-                          color: Color(0xFF598BED),), // ic
-                        SizedBox(width: 10,),// on
-                        Text("Get Help",
-                          style: TextStyle(
-                              color: Color(0xFF598BED)
-                          ),), // text
                       ],
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    padding: const EdgeInsets.only(left: 20),
-                    margin: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          color: const Color(0xFF598BED),
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Image(
+                        image: AssetImage('assets/card 1.png'),
+                        fit: BoxFit.fill,
+                        height: 140,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10.0),
+                        child: Text(
+                          'WORKING PARENTS',
+                          style:
+                          TextStyle(fontSize: 12, color: Color(0xFF5988ED)),
                         ),
-                        borderRadius: BorderRadius.circular(8)
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: const <Widget>[
-                        Icon(Icons.import_contacts,
-                          color: Color(0xFF598BED),), // ic
-                        SizedBox(width: 10,),// on
-                        Text("Learn",
-                          style: TextStyle(
-                              color: Color(0xFF598BED)
-                          ),), // text
-                      ],
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    padding: const EdgeInsets.only(left: 20),
-                    margin: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          color: const Color(0xFF598BED),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Flexible(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 10.0, right: 10),
+                          child: Text(
+                            'Understanding Human behaviour',
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
                         ),
-                        borderRadius: BorderRadius.circular(8)
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: const <Widget>[
-                        Icon(Icons.insert_chart,
-                          color: Color(0xFF598BED),), // ic
-                        SizedBox(width: 10,),// on
-                        Text("DD Tracker",
-                          style: TextStyle(
-                              color: Color(0xFF598BED)
-                          ),), // text
-                      ],
-                    ),
+                      ),
+                      SizedBox(height: 20,),
+                      Padding(padding: EdgeInsets.only(left: 10),
+                        child: Text('12 lessons', style: TextStyle(fontSize: 12, color: Color(0xFF6D747A)),),
+                      )
+                    ],
                   ),
                 ),
               ],
